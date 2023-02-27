9dbf0b6c18f0c74aff094bdf8874982a4eaa6fc4
Alison Mazzarella-Woelzl <104347449+AlisonMazzarella@users.noreply.github.com>
2023-02-27 11:00:38 +0100

    Files added for project

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MvcMovie.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
+
+
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "Meet the Mormons",
+                    ReleaseDate = DateTime.Parse("2014-10-10"),
+                    Genre = "Documentary",
+                    Rating = "PG",
+                    Price = 7.50M
+                },
+                new Movie
+                {
+                    Title = "The Saratov Approach",
+                    ReleaseDate = DateTime.Parse("2013-10-15"),
+                    Genre = "Thriller",
+                    Rating = "PG-13",
+                    Price = 8.50M
+                },
+                new Movie
+                {
+                    Title = "Johnny Lingo",
+                    ReleaseDate = DateTime.Parse("1969-2-23"),
+                    Genre = "Romantic Comedy",
+                    Rating = "G",
+                    Price = 9.50M
+                },
+                new Movie
+                {
+                    Title = "17 Miracles",
+                    ReleaseDate = DateTime.Parse("2011-6-03"),
+                    Genre = "Western",
+                    Rating = "PG",
+                    Price = 6.50M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
